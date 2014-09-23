66d0ecf8233eba19636a0b9e35e39a6724c0ea10
yass8n <yass8n@yahoo.com>
2014-09-23 00:50:43 -0700

    switched webApp from using namespacing to using

--- webapp/angular/controllers/mainController.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module("app",['ngRoute', 'ngAnimate']);
-
-app.controller('mainController',function($scope){
-	this.test = "This is an angular test";
-	$scope.test = "This is an angular test";
-});
--- webapp/angular/js/recipe.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	$('.food').click(function(){
-		$(this).remove();
-	});
-});
