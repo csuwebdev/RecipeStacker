8c2cb07340942832b691d2d38715894c7005f1fa
yass8n <yass8n@yahoo.com>
2014-09-26 00:16:31 -0700

    results from API call are now showing in 'recipes' field

--- public/angular/controllers/controllers.js
+++ public/angular/controllers/controllers.js
@@ -2,96 +2,79 @@ var TheControllers = angular.module('TheControllers', []);
 
 TheControllers.controller('RecipeController', ['$scope', '$http', function($scope, $http) {
  $scope.query_result = [
-  {
-    name:"Pizza",
+  { 
+    name: "steak"
+  },
+  { 
+    name: "eggs"
   },
   {
-    name:"Bacon",
+    name: "buttermilk"
   },
   {
-    name:"Beef",
+    name: "flour"
   },
   {
-    name:"Broccili",
+    name:"vegetable oil"
   },
   {
-    name:"Pork",
+    name:"garlic"
   },
   {
-    name:"Pizza",
+    name:"cognac"
   },
   {
-    name:"Bacon",
+    name:"cumin seed"
   },
   {
-    name:"Beef",
-  },
-  {
-    name:"Broccili",
-  },
-  {
-    name:"Pork",
+    name:"pizza"
   }
 ]
+$scope.chosen_ingredients=[]
+$scope.recipes=[]
 
-// $scope.chosen_ingredients=[]
-$scope.chosen_ingredients=[
-  {
-    name:"Cake"
-  }
-]
-$scope.recipes=[
-  {
-    name:"Roast Beef Sandwhich"
-  }
-]
-
-$scope.removeIngredientAndAdd = function(name, container, index){
+$scope.switchAndDisplay = function(name, container, index){
     container.splice(index,1);
-    // $scope.push($scope.chosen_ingredients[index]);
-    // console.log("asdasd");
      $scope.chosen_ingredients.push(name);
-     // console.log($scope.chosen_ingredients);
-     // console.log("Asdasd");
+     $scope.recipes.length = 0;
+     $scope.displayRecipes();
 }
-$scope.removeIngredient = function(container, index){
+$scope.remove = function(container, index){
     container.splice(index,1);
+    //remove all recipes because we have no ingredients chosen or we have removen an ingredient and we need to empty the recipe array to render a new recipe array later
+   if($scope.chosen_ingredients.length == 0 || container != $scope.recipes) 
+     $scope.recipes.length = 0;
+
+   //if we arent removing a recipe, display the new recipe list
+   if (container != $scope.recipes) { 
+     $scope.displayRecipes();
+   }
 }
-//app id 885488fb
-//app key 453ae9fd4d29a72598c6368d9734d3fa
-//$http.defaults.headers.common["X-Custom-Header"] = "Angular.js"
-//need to use factory http://tylermcginnis.com/angularjs-factory-vs-service-vs-provider/
-//stack overflow answer to api yummly call http://stackoverflow.com/questions/13464619/how-do-i-interpret-json-if-jquery-thinks-it-is-receiving-a-jsonp-request
-$scope.displayRecipes = function() {
-//    http://api.yummly.com/v1/api/recipes?_app_id=YOUR_ID&_app_key=YOUR_APP_KEY&q=onion+soup
-// &allowedIngredient[]=garlic&allowedIngredient[]=cognac
-      $http.get('http://api.yummly.com/v1/api/recipes?_app_id=885488fb&_app_key=453ae9fd4d29a72598c6368d9734d3fa&allowedIngredient[]=garlic&allowedIngredient[]=cognac',
-        {'X-Yummly-App-ID' : '885488fb', 'X-Yummly-App-Key' : '453ae9fd4d29a72598c6368d9734d3fa', 'dataType': 'jsonp', headers:{
-                // 'Access-Control-Allow-Origin': '*',
-                // 'Access-Control-Allow-Methods': 'GET',
-                // 'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With'
-            }})
-      //   , { headers: {
-      //     'Authorization': 'Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=='
-      //   }
-      // });
-    //   , {
-    //   'name' : $scope.formName, 
-    //   'author' : $scope.formAuthor, 
-    //   'quote' : $scope.formQuote
-    // })
-    .success(function(data) {
-      // $scope.quotes = data;
-      // $scope.formAuthor = ''; // clear the form so our user is ready to enter another
-      // $scope.formName = ''; // clear the form so our user is ready to enter another
-      // $scope.formQuote = ''; // clear the form so our user is ready to enter another
-      console.log(data);
-    })
-    .error(function(data) {
-      console.log('Error: ' + data);
+$scope.removeAll = function(container){
+    var index = 0;
+    container.forEach(function(element){
+      container.splice(index,1);
     });
+}
+$scope.displayRecipes = function() {
+  //if there are chosen ingredients present, execute the api call
+  if ($scope.chosen_ingredients.length) {
+      var http = 'http://api.yummly.com/v1/api/recipes?_app_id=885488fb&_app_key=453ae9fd4d29a72598c6368d9734d3fa'
+      $scope.chosen_ingredients.forEach(function(element){
+        http += '&allowedIngredient[]='+element.name
+      });
+      $http.get(http, {'dataType': 'jsonp'})
+        .success(function(data) {
+          data.matches.forEach(function(recipe){
+            $scope.recipes.push({name:recipe.recipeName});
+        });
+          console.log(data);
+      })
+      .error(function(data) {
+        console.log('Error');
+      });
+    }
   }
-//   ;
 }]);
 
 TheControllers.controller('LandingController', ['$scope', function($scope) {
