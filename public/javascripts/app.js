4e6266ec083f7a994c6ab77911b0e8fc541b0df8
Billy <wsteffenhagen@mail.csuchico.edu>
2014-12-07 12:38:37 -0800

    Updating Billy's picture.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -715,6 +715,36 @@ myApp.config(['$routeProvider', function($routeProvider) {
 }]);
 
 
+var recipeService = angular.module('recipeService', ['ngResource', 'ingredientService']);
+
+recipeService.service('detailsService', function(){
+  var recipeData= "";
+  var ingredients = []; 
+
+  var setData = function(data) {
+      ingredients = [];
+      for (var i =0; i < data.ingredientLines.length; i ++){
+        if (data.ingredientLines[i] != data.ingredientLines[i+1])
+          ingredients.push(data.ingredientLines[i]);
+      }
+      recipeData = data;
+      return true;
+  }
+  var getIngredients = function(){
+    return ingredients;
+  }
+
+  var getData = function(){
+      return recipeData;
+  }
+
+  return {
+    setData: setData,
+    getData: getData,
+    getIngredients: getIngredients
+  };      
+});
+
 var theDataService = angular.module('theDataService', ['ngResource', 'theIngredientService']);
 
 
