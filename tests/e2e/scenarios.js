767006e82e6511b3344d1237b2185b4775ad4b87
Jayd Saucedo <saurdo@gmail.com>
2014-12-09 20:25:46 -0800

    e2e test changes

--- tests/e2e/scenarios.js
+++ tests/e2e/scenarios.js
@@ -1,6 +1,12 @@
 describe('Recipeasy App', function() {
 
   describe('Recipeasy search view', function() {
+    var ingredientList = element.all(by.repeater('ingredient in query_result'));
+    var chosenIngredientList = element.all(by.repeater('ingredient in chosen_ingredients'));
+    var topRecipes = element.all(by.repeater('recipe in topRecipes'));
+    var recipes = element.all(by.repeater('recipe in recipes'));
+
+    var query = element(by.model('match'));
 
     beforeEach(function() {
       browser.get('#/app/search');
@@ -8,22 +14,27 @@ describe('Recipeasy App', function() {
 
 
     it('should filter the ingredient list as a user types into the search box', function() {
-
-      var ingredientList = element.all(by.repeater('ingredient in query_result'));
-      var query = element(by.model('match'));
-
       query.sendKeys('large free range egg');
       expect(ingredientList.count()).toBe(1);
 
     });
 
     it('should add the ingredients the user selects in the search box to the chosen ingredients list', function() {
-      var chosenIngredientList = element.all(by.repeater('ingredient in chosen_ingredients'));
-      var query = element(by.model('match'));
       query.sendKeys('large free range egg');
       query.sendKeys(protractor.Key.ENTER);
       expect(chosenIngredientList.count()).toBe(1);
     });
+  
+  it('Should search the API for relevant recipes and display top recipes', function() {
+      query.sendKeys('milk');
+      query.sendKeys(protractor.Key.ENTER);
+      expect(topRecipes.count()).toBe(2);
+    });
 
+  it('Should search the API for relevant recipes and display list of recipes', function() {
+      query.sendKeys('cheese');
+      query.sendKeys(protractor.Key.ENTER);
+      expect(recipes.count()).toBe(9);
+    });
   });
 });
