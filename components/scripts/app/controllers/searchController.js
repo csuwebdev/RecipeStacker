6e69b438dbbcca6b6e97812fbb7ff6ae99f452d1
Erik Mellum <emellum@student.mail.csuchico.edu>
2015-02-07 17:55:07 -0800

    Fixed dialog popup

--- components/scripts/app/controllers/searchController.js
+++ components/scripts/app/controllers/searchController.js
@@ -175,7 +175,6 @@ searchController.controller('SearchController', ['$scope','$http', '$window','de
           excludedIngredients.push(ingredient.name);
 
       });
-      alert("YOUR MEAL IS: " + $scope.meal);
       var postObject = {"ingredients" : allowedIngredients, "excluded" : excludedIngredients, "meal" : $scope.meal};
       $http.post(url, postObject).success(function(data) {
         $scope.dataArray = data;
